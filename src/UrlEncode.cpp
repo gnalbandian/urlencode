03cd6598530a788096db4bce23d253a68c2cf314
Gonzalo Nalbandian <gonzalonalbandian@hotmail.com>
2025-04-08 19:41:28 -0300

    Ported to esp-idf. Removed Arduino dependencies.

--- src/UrlEncode.cpp
+++ src/UrlEncode.cpp
@@ -1,23 +1,60 @@
 #include "UrlEncode.h"
+#include <string>
 
-String urlEncode(const char *msg) {
+
+
+#include "UrlEncode.h"
+// #include <cstdio>  // For snprintf
+#include <string.h>
+
+std::string urlEncode(const char *msg) {
   const char *hex = "0123456789ABCDEF";
-  String encodedMsg = "";
+  std::string encodedMsg;
+  // Optionally reserve capacity: 3 times the input length (worst case)
+  encodedMsg.reserve(strlen(msg) * 3);
 
   while (*msg != '\0') {
-    if (
-        ('a' <= *msg && *msg <= 'z') || ('A' <= *msg && *msg <= 'Z') || ('0' <= *msg && *msg <= '9') || *msg == '-' || *msg == '_' || *msg == '.' || *msg == '~') {
-      encodedMsg += *msg;
-    } else {
-      encodedMsg += '%';
-      encodedMsg += hex[(unsigned char)*msg >> 4];
-      encodedMsg += hex[*msg & 0xf];
-    }
-    msg++;
+      // Check if the character is unreserved
+      if (('a' <= *msg && *msg <= 'z') ||
+          ('A' <= *msg && *msg <= 'Z') ||
+          ('0' <= *msg && *msg <= '9') ||
+          *msg == '-' || *msg == '_' || *msg == '.' || *msg == '~') {
+          encodedMsg.push_back(*msg);
+      } else {
+          // Percent-encode reserved or unsafe characters
+          encodedMsg.push_back('%');
+          encodedMsg.push_back(hex[(unsigned char)*msg >> 4]);
+          encodedMsg.push_back(hex[(unsigned char)*msg & 0x0F]);
+      }
+      msg++;
   }
   return encodedMsg;
 }
 
-String urlEncode(String msg) {
+// Overload for std::string input
+std::string urlEncode(const std::string &msg) {
   return urlEncode(msg.c_str());
 }
+
+
+// String urlEncode(const char *msg) {
+//   const char *hex = "0123456789ABCDEF";
+//   String encodedMsg = "";
+
+//   while (*msg != '\0') {
+//     if (
+//         ('a' <= *msg && *msg <= 'z') || ('A' <= *msg && *msg <= 'Z') || ('0' <= *msg && *msg <= '9') || *msg == '-' || *msg == '_' || *msg == '.' || *msg == '~') {
+//       encodedMsg += *msg;
+//     } else {
+//       encodedMsg += '%';
+//       encodedMsg += hex[(unsigned char)*msg >> 4];
+//       encodedMsg += hex[*msg & 0xf];
+//     }
+//     msg++;
+//   }
+//   return encodedMsg;
+// }
+
+// String urlEncode(String msg) {
+//   return urlEncode(msg.c_str());
+// }
--- src/UrlEncode.h
+++ src/UrlEncode.h
@@ -1,20 +1,41 @@
 #ifndef _PLAGEOJ_URLENCODE_H
 #define _PLAGEOJ_URLENCODE_H
 
-#include <Arduino.h>
+
+#include <string>
+
+// #include <cstddef>
 
 /**
- * Percent-encodes a string.
- * @param msg UTF-8 string to encode.
- * @returns Percent-encoded string.
+ * Percent-encodes a UTF-8 string.
+ *
+ * @param input      Null-terminated input string.
+ * @param output     Output buffer for the percent-encoded string.
+ * @param outputSize Size of the output buffer in bytes.
+ * @return           Number of characters written (excluding the terminating null),
+ *                   or -1 if the output buffer is too small.
  */
-String urlEncode(const char *msg);
+std::string urlEncode(const std::string &msg);
+std::string urlEncode(const char *msg);
 
-/**
- * Percent-encodes a string.
- * @param msg UTF-8 string to encode.
- * @returns Percent-encoded string.
- */
-String urlEncode(String msg);
+#endif // URL_ENCODE_HPP
 
-#endif
+// #endif // URL_ENCODE_H
+
+// // #include <Arduino.h>
+
+// /**
+//  * Percent-encodes a string.
+//  * @param msg UTF-8 string to encode.
+//  * @returns Percent-encoded string.
+//  */
+// String urlEncode(const char *msg);
+
+// /**
+//  * Percent-encodes a string.
+//  * @param msg UTF-8 string to encode.
+//  * @returns Percent-encoded string.
+//  */
+// String urlEncode(String msg);
+
+// #endif
